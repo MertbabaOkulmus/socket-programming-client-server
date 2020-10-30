9033ad7d39971b4bc048f55cb32028c4d8435833
Batuhansrp <68540266+Batuhansrp@users.noreply.github.com>
2020-10-30 16:06:43 +0300

    Add files via upload

---
+++ Client/WindowsFormsApp1/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpClient client;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += Client_DataReceived;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            //Connect to server
+            client.Connect(txtHost.Text, Convert.ToInt32(txtPort.Text));
+        }
+        private void Client_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            //Update message to txtStatus
+            txtStatus.Invoke((MethodInvoker)delegate ()
+            {
+                txtStatus.Text += e.MessageString;
+            });
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            client.WriteLineAndGetReply(txtMessage.Text, TimeSpan.FromSeconds(3));
+        }
+    }
+}
